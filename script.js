53360acc21a061215d8e601ebd042c7b01a10331
Emerson Duarte <frenow@gmail.com>
2026-02-04 13:54:41 -0300

    Update API endpoint for fetching Bitcoin data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function fetchData() {
-    const res = await fetch ("https://api.coincap.io/v2/assets/bitcoin");
+    const res = await fetch ("https://rest.coincap.io/v3/assets/bitcoin?apiKey=dd120d92e85256327c959e2e8edd594a4594528dea0b6fde58a626739d10e149");
     const record = await res.json();
     console.log(record);
     document.getElementById("symbol").innerHTML=record.data.symbol;
